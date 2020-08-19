0e326218f3ba1f55114eb79eefdebad3dd427a0a
Masahiro Sakuta <masahiro.sakuta@gmail.com>
2020-08-20 03:30:38 +0900

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+extern crate image;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
+
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use wasm_bindgen::prelude::*;
+use render::{RenderColor,
+    UVMap,
+    RenderMaterial, RenderPattern,
+    RenderObject, RenderSphere, RenderFloor,
+    RenderEnv,
+    render, render_frames};
+use vec3::Vec3;
+
+mod render;
+mod vec3;
+mod quat;
+mod modutil;
+mod pixelutil;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn helloworld() -> String {
+    String::from("Hello world from Rust!")
+}
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn example() {
+    log("Log from Rust!!!");
+}
+
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 | 1 => n,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+#[wasm_bindgen]
+pub fn render_func() -> Vec<u8> {
+    let xmax = 320;
+    let ymax = 240;
+    let xfov = 1.;
+    let yfov = 1.;
+    let width= xmax;
+    let height = ymax;
+    let thread_count = 1;
+    use std::f32::consts::PI;
+
+    let mut materials: HashMap<String, Arc<RenderMaterial>> = HashMap::new();
+
+    let floor_material = Arc::new(RenderMaterial::new("floor".to_string(),
+    RenderColor::new(1.0, 1.0, 0.0), RenderColor::new(0.0, 0.0, 0.0),  0, 0., 0.0)
+    .pattern(RenderPattern::RepeatedGradation)
+    .pattern_scale(300.)
+    .pattern_angle_scale(0.2)
+    .texture_ok("bar.png"));
+    materials.insert("floor".to_string(), floor_material);
+
+    let objects: Vec<RenderObject> = vec!{
+        /* Plane */
+            RenderObject::Floor(
+                RenderFloor::new_raw(materials.get("floor").unwrap().clone(),       Vec3::new(  0.0, -300.0,  0.0),  Vec3::new(0., 1., 0.))
+                .uvmap(UVMap::ZX),
+            ),
+            // RenderFloor::new (floor_material,       Vec3::new(-300.0,   0.0,  0.0),  Vec3::new(1., 0., 0.)),
+        /* Spheres */
+        //     RenderSphere::new(mirror_material.clone(), 80.0, Vec3::new(   0.0, -30.0,172.0)),
+        //     RenderSphere::new(mirror_material, 80.0, Vec3::new(   -200.0, -30.0,172.0)),
+        //     RenderSphere::new(red_material, 80.0, Vec3::new(-200.0,-200.0,172.0)),
+        // /*	{80.0F,  70.0F,-200.0F,150.0F, 0.0F, 0.0F, 0.8F, 0.0F, 0.0F, 0.0F, 0.0F,24, 1., 1., {1.}},*/
+        //     RenderSphere::new(transparent_material, 100.0, Vec3::new(  70.0,-200.0,150.0)),
+        /*	{000.F, 0.F, 0.F, 1500.F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F,24, 0, 0},*/
+        /*	{100.F, -70.F, -150.F, 160.F, 0.0F, 0.5F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F,24, .5F, .2F},*/
+        };
+    
+    fn bgcolor(ren: &RenderEnv, direction: &Vec3) -> RenderColor{
+        RenderColor::new(1., 0., 0.)
+    }
+
+    let mut data = vec![0u8; 3 * width * height];
+
+    for y in 0..height {
+        for x in 0..width {
+            data[(x + y * width) * 3    ] = ((x) * 255 / width) as u8;
+            data[(x + y * width) * 3 + 1] = ((y) * 255 / height) as u8;
+            data[(x + y * width) * 3 + 2] = ((x + y) % 32 + 32) as u8;
+        }
+    }
+
+    let mut putpoint = |x: i32, y: i32, fc: &RenderColor| {
+        data[(x as usize + y as usize * width) * 3    ] = (fc.r * 255.).min(255.) as u8;
+        data[(x as usize + y as usize * width) * 3 + 1] = (fc.g * 255.).min(255.) as u8;
+        data[(x as usize + y as usize * width) * 3 + 2] = (fc.b * 255.).min(255.) as u8;
+    };
+
+    let mut ren: RenderEnv = RenderEnv::new(
+        Vec3::new(0., -150., -300.), /* cam */
+        Vec3::new(0., -PI / 2., -PI / 2.), /* pyr */
+        xmax as i32,
+        ymax as i32, /* xres, yres */
+        xfov,
+        yfov, /* xfov, yfov*/
+        //pointproc: putpoint, /* pointproc */
+        bgcolor, /* bgproc */
+    )
+    .materials(materials)
+    .objects(objects)
+    .light(Vec3::new(50., 60., -50.))
+    .use_raymarching(false)
+    .glow_effect(None);
+    log(&format!("pyr: {}, {}, {}", ren.camera.pyr.x, ren.camera.pyr.y, ren.camera.pyr.z));
+
+    render(&ren, &mut putpoint, thread_count);
+
+    log(&format!("data: {}, {}", data[0], data.len()));
+
+    data
+}
---
+++ test.js
@@ -0,0 +1,45 @@
+import init, { fibonacci, render_func } from './pkg/helloworld.js'
+
+function fibonacciInJs(n) {
+  if (n <= 1) return n
+  return fibonacciInJs(n - 1) + fibonacciInJs(n - 2)
+}
+
+async function run() {
+  await init()
+  const num = 20
+
+  console.time('Fibonnaci in rust')
+  const fibRust = fibonacci(num)
+  console.timeEnd('Fibonnaci in rust')
+
+  console.time('Fibonnaci in JS')
+  const fibJS = fibonacciInJs(num)
+  console.timeEnd('Fibonnaci in JS')
+
+  console.time('Fibonnaci in JS')
+  const buf = render_func()
+  console.timeEnd('Fibonnaci in JS')
+
+  console.log(buf)
+
+  const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.createImageData(320, 240);
+
+    // Iterate through every pixel
+    for (let i = 0; i < imageData.data.length; i += 1) {
+    // Modify pixel data
+    imageData.data[4 * i + 0] = buf[3 * i + 0];  // R value
+    imageData.data[4 * i + 1] = buf[3 * i + 1];    // G value
+    imageData.data[4 * i + 2] = buf[3 * i + 2];  // B value
+    imageData.data[4 * i + 3] = 255;  // A value
+    }
+
+    // Draw image data to the canvas
+    ctx.putImageData(imageData, 0, 0);
+
+  document.getElementById("text").innerHTML = `Fib ${num}:  Rust ${fibRust} - JS ${fibJS}`
+}
+
+run()
