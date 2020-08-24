b5875d0bd27eb8fad859d85821dfb133eefef742
msakuta <msakuta@users.noreply.github.com>
2020-08-24 22:25:20 +0000

    deploy: 4b973080610b063172933ad1122dd3396af9cd65

--- pkg/helloworld_bg.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function helloworld(a: number): void;
-export function render_func(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_malloc(a: number): number;
